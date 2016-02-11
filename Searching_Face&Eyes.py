65772b272c00c68f1d790aa9259f7a2df5f5a234
Zhicong Zang <zzc19920809@yahoo.com>
2016-02-11 14:10:32 -0600

    no message

--- Searching_Face&Eyes.py
+++ Searching_Face&Eyes.py
@@ -5,10 +5,12 @@ import datetime
 cv2.namedWindow("test")
 cap = cv2.VideoCapture(0)
 success,frame = cap.read()
-faceClassifier = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+faceClassifier = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 eyeClassifier = cv2.CascadeClassifier("haarcascade_eye_tree_eyeglasses.xml")
-leftEyeClassifier = cv2.CascadeClassifier("haarcascade_lefteye_2splits.xml")
-rightEyeClassifier = cv2.CascadeClassifier("haarcascade_righteye_2splits.xml")
+noseClassifier = cv2.CascadeClassifier("haarcascade_mcs_nose.xml")
+
+#leftEyeClassifier = cv2.CascadeClassifier("haarcascade_lefteye_2splits.xml")
+#rightEyeClassifier = cv2.CascadeClassifier("haarcascade_righteye_2splits.xml")
 
  
 while success:
@@ -21,24 +23,32 @@ while success:
 	minSize = (w/divisor,h/divisor)
 	minEyeSize = (w/20,h/20)
 	maxEyeSize = (w/10,h/10)
+	noseRects = noseClassifier.detectMultiScale(image,1.2,2,cv2.CASCADE_SCALE_IMAGE,minEyeSize,maxEyeSize)
+	if len(noseRects) > 0:
+		for noseRect in noseRects:
+			nx,ny,nw,nh=noseRect
+			cv2.circle(frame,(nx + nw/2,ny + nh/2),min(nw/2,nh/2),(0,255,0))
+	#else: 
+		#print(datetime.datetime.now().strftime('%Y/%m/%d %H:%m:%S.%f')[:-3] + " Not Found nose")
+	eyeRects = eyeClassifier.detectMultiScale(image,1.2,2,cv2.CASCADE_SCALE_IMAGE,minEyeSize,maxEyeSize)
+	if len(eyeRects) > 0:
+		for eyeRect in eyeRects:
+			ex,ey,ew,eh=eyeRect
+			cv2.circle(frame,(ex + ew/2,ey + eh/2),min(ew/2,eh/2),(0,0,255))
+	#else: 
+		#print(datetime.datetime.now().strftime('%Y/%m/%d %H:%m:%S.%f')[:-3] + " Not Found eyes")
 	faceRects = faceClassifier.detectMultiScale(image,1.2,2,cv2.CASCADE_SCALE_IMAGE,minSize)
 	if len(faceRects) > 0:
 		for faceRect in faceRects:
 			x,y,w,h=faceRect
 			#Face
 			cv2.circle(frame,(x + w/2,y + h/2),min(w/2,h/2),(255,0,0))
-			#Mouse
 			cv2.rectangle(frame,(x+3*w/8,y+3*h/4),(x+5*w/8,y+7*h/8),(255,0,0))
-			eyeRects = eyeClassifier.detectMultiScale(image,1.2,2,cv2.CASCADE_SCALE_IMAGE,minEyeSize,maxEyeSize)
-			if len(eyeRects) > 0:
-				for eyeRect in eyeRects:
-					ex,ey,ew,eh=eyeRect
-					cv2.circle(frame,(ex + ew/2,ey + eh/2),min(ew/2,eh/2),(0,0,255))
-			else: 
-				print(datetime.datetime.now().strftime('%Y/%m/%d %H:%m:%S.%f')[:-3] + " Not Found eyes")
+			
 
 			#cv2.circle(frame,(x+w/4,y+h/3),min(w/8,h/8),(255,0,0))
 			#cv2.circle(frame,(x+3*w/4,y+h/3),min(w/8,h/8),(255,0,0))
+			
 			
 	cv2.imshow("test",frame) 
 	key = cv2.waitKey(10)
